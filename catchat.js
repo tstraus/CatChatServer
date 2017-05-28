d01ee3a2d872274297f70a0873e324de49cfb098
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2017-05-28 13:44:22 -0400

    handle all uuid on server side and check if the username is already taken when joining

--- catchat.js
+++ catchat.js
@@ -1,48 +1,12 @@
 const uuid = require("uuid/v4");
-var WebSocketServer = require("uws").Server;
+const WebSocketServer = require("uws").Server;
+const chalk = require("chalk");
 
 var wss = new WebSocketServer({ port: 1234 });
 var connections = new Map(); // uuid to websocket connection map
 var users = new Map(); // username to uuid map
 
-function onMessage(message) {
-    var msg = JSON.parse(message);
-
-    if (msg.type === "join" && connections.has(msg.uuid)) {
-        console.log(msg.user + " joined");
-        users.set(msg.user, msg.uuid);
-    }
-
-    else if (msg.type === "msg" && connections.has(msg.uuid)) {
-        try {
-            console.log(msg.user + " -> " + msg.data.dest + ": " + msg.data.msg);
-
-            var output = {
-                type: "msg",
-                uuid: users.get(msg.data.dest), // uuid of the desination, verification
-                user: msg.user, // sender username
-                data: msg.data.msg
-            };
-
-           connections.get(output.uuid).send(JSON.stringify(output));
-        } catch (err) {
-            console.log("Could not find the destination");
-        }
-    }
-
-    else {
-        console.log("Received invalid message type");
-    }
-}
-
-function onClose() {
-
-}
-
 wss.on("connection", function(ws) {
-    ws.on("message", onMessage);
-    ws.on("close", onClose);
-
     ws.uuid = uuid();
     connections.set(ws.uuid, ws);
     console.log("connection: " + ws.uuid);
@@ -53,4 +17,48 @@ wss.on("connection", function(ws) {
     };
 
     ws.send(JSON.stringify(id));
+
+    // ws callbacks
+    ws.on("message", function(message) {
+        var msg = JSON.parse(message);
+
+        if (msg.type === "join") {
+            if (!users.has(msg.user)) {
+                console.log(msg.user + " joined as: " + ws.uuid);
+                users.set(msg.user, ws.uuid);
+                ws.user = msg.user;
+            }
+        }
+
+        else if (msg.type === "msg") {
+            try {
+                console.log(ws.user + " -> " + msg.user + ": " + msg.data);
+
+                var output = {
+                    type: "msg",
+                    user: ws.user,
+                    data: msg.data
+                };
+
+                connections.get(users.get(msg.user)).send(JSON.stringify(output));
+            } catch (err) {
+                console.log(chalk.yellow("Could not find the destination: ") + msg.user);
+            }
+        }
+
+        else {
+            console.log(chalk.yellow("Received invalid message type: ") + msg.type);
+        }
+    });
+    
+    ws.on("close", function() {
+        if ("user" in ws) {
+            console.log(ws.user + " disconnected");
+        } else {
+            console.log(ws.uuid + " disconnected");
+        }
+
+        users.delete(ws.user);
+        connections.delete(ws.uuid);
+    });
 });
