5a4c4c76e1c90a6d1794e7020189ce62adf299f6
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2017-06-01 17:32:49 -0400

    added standard js

--- catchat.js
+++ catchat.js
@@ -1,64 +1,62 @@
-const uuid = require('uuid/v4');
-const WebSocketServer = require('uws').Server;
-const chalk = require('chalk');
+/* eslint-env node */
 
-const wss = new WebSocketServer({ port: 1234 });
-let connections = new Map(); // uuid to websocket connection map
-let users = new Map(); // username to uuid map
+const uuid = require('uuid/v4')
+const WebSocketServer = require('uws').Server
+const chalk = require('chalk')
 
-wss.on('connection', function(ws) {
-    ws.uuid = uuid();
-    connections.set(ws.uuid, ws);
-    console.log('connection: ' + ws.uuid);
+const wss = new WebSocketServer({ port: 1234 })
+let connections = new Map() // uuid to websocket connection map
+let users = new Map() // username to uuid map
 
-    const id = {
-        type: 'uuid',
-        uuid: ws.uuid
-    };
+wss.on('connection', function (ws) {
+  ws.uuid = uuid()
+  connections.set(ws.uuid, ws)
+  console.log('connection: ' + ws.uuid)
 
-    ws.send(JSON.stringify(id));
+  const id = {
+    type: 'uuid',
+    uuid: ws.uuid
+  }
+
+  ws.send(JSON.stringify(id))
 
     // ws callbacks
-    ws.on('message', function(message) {
-        const msg = JSON.parse(message);
+  ws.on('message', function (message) {
+    const msg = JSON.parse(message)
 
-        if (msg.type === 'join') {
-            if (!users.has(msg.user)) {
-                console.log(msg.user + ' joined as: ' + ws.uuid);
-                users.set(msg.user, ws.uuid);
-                ws.user = msg.user;
-            }
+    if (msg.type === 'join') {
+      if (!users.has(msg.user)) {
+        console.log(msg.user + ' joined as: ' + ws.uuid)
+        users.set(msg.user, ws.uuid)
+        ws.user = msg.user
+      }
+    } else if (msg.type === 'msg') {
+      try {
+        console.log(ws.user + ' -> ' + msg.user + ': ' + msg.data)
+
+        const output = {
+          type: 'msg',
+          user: ws.user,
+          data: msg.data
         }
 
-        else if (msg.type === 'msg') {
-            try {
-                console.log(ws.user + ' -> ' + msg.user + ': ' + msg.data);
+        connections.get(users.get(msg.user)).send(JSON.stringify(output))
+      } catch (err) {
+        console.log(chalk.yellow('Could not find the destination: ') + msg.user)
+      }
+    } else {
+      console.log(chalk.yellow('Received invalid message type: ') + msg.type)
+    }
+  })
 
-                const output = {
-                    type: 'msg',
-                    user: ws.user,
-                    data: msg.data
-                };
+  ws.on('close', function () {
+    if ('user' in ws) {
+      console.log(ws.user + ' disconnected')
+    } else {
+      console.log(ws.uuid + ' disconnected')
+    }
 
-                connections.get(users.get(msg.user)).send(JSON.stringify(output));
-            } catch (err) {
-                console.log(chalk.yellow('Could not find the destination: ') + msg.user);
-            }
-        }
-
-        else {
-            console.log(chalk.yellow('Received invalid message type: ') + msg.type);
-        }
-    });
-    
-    ws.on('close', function() {
-        if ('user' in ws) {
-            console.log(ws.user + ' disconnected');
-        } else {
-            console.log(ws.uuid + ' disconnected');
-        }
-
-        users.delete(ws.user);
-        connections.delete(ws.uuid);
-    });
-});
+    users.delete(ws.user)
+    connections.delete(ws.uuid)
+  })
+})
